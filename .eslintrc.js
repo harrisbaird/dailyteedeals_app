360fb14b8d0a5d6736c0b19e304662c70e2f2a9a
harrisbaird <mydancake@gmail.com>
2017-02-22 01:22:14 +0000

    Add eslint dependancies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,26 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
+  "parser": "babel-eslint",
+  "extends":  [
+    "standard",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+  ],
+  "rules": {
+    "flowtype-errors/show-errors": 2,
+    "react/prop-types": 0
+  },
+  "plugins": [
+    "standard",
+    "promise",
+    "react",
+    "react-native",
+    "flowtype-errors",
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+
 };
