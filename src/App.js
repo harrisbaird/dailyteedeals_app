72049d8b834eeaa4c27781286afe79ddb5c67cc9
harrisbaird <mydancake@gmail.com>
2017-02-24 05:39:44 +0000

    Delay until settings store is rehydrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 /* @flow */
 
 import React from 'react'
-import { View, StatusBar, StyleSheet } from 'react-native'
+import { View, StatusBar, StyleSheet, AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
 import { COLOUR_HEADER_BG } from './config/constants'
 import store from './stores'
 import Nav from './Nav'
 
-const App = () => (
-  <View style={styles.container}>
-    <StatusBar backgroundColor={COLOUR_HEADER_BG} barStyle='light-content' />
-    <Provider store={store}>
-      <Nav />
-    </Provider>
-  </View>
-)
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { rehydrated: false }
+  }
+
+  componentWillMount(){
+    persistStore(store, {storage: AsyncStorage, whitelist: ['settings']}, () => {
+      this.setState({ rehydrated: true })
+    })
+  }
+
+  render() {
+    // This prevents the deals grid from updating multiple times during
+    // while the settings store is rehydrating.
+    if(!this.state.rehydrated){
+      return null
+    }
+
+    return (
+      <View style={styles.container}>
+        <StatusBar backgroundColor={COLOUR_HEADER_BG} barStyle='light-content' />
+        <Provider store={store}>
+          <Nav />
+        </Provider>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   }
 })
-
-export default App
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,7 @@
 /* @flow */
 
-import { AsyncStorage } from 'react-native'
 import { compose, createStore, applyMiddleware } from 'redux'
-import { persistStore, autoRehydrate } from 'redux-persist'
+import { autoRehydrate } from 'redux-persist'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
@@ -14,7 +13,5 @@ const store = createStore(
     autoRehydrate()
   )
 )
-
-persistStore(store, {storage: AsyncStorage, whitelist: ['settings']})
 
 export default store
